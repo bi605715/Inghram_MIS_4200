d53bb89c155a897ec56f562b738e3f1e475b6a13
Brooke Inghram <bi605715@ohio.edu>
2019-09-11 11:40:29 -0400

    commit

---
+++ Models/order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inghram_MIS_4200.Models
+{
+    public class order
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+
+}
+}
